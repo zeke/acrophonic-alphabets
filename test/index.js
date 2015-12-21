cf049e74726e55c8f515c11df8ea74af76802139
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-20 18:07:58 -0600

    add a unique id string to each alphabet object

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 const alphabets = require('..')
 const assert = require('assert')
+const uniq = require('lodash.uniq')
 
 describe('acrophonic-alphabets', () => {
   it('is an array', () => {
@@ -10,5 +11,11 @@ describe('acrophonic-alphabets', () => {
 
   it('always has 26 words', () => {
     assert(alphabets.every(a => a.words.length === 26))
+  })
+
+  it('always has a uniq id', () => {
+    var ids = alphabets.map(a => a.id)
+    assert(ids.every(id => id.length > 0))
+    assert.deepEqual(ids, uniq(ids))
   })
 })
